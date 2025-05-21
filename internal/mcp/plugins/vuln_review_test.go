2b1cd75f41489e81d4e4310e3f3a08ca4d47aaeb
zhuque <zhuque@tencent.com>
2025-05-21 09:53:57 +0800

    add vuln review test

--- internal/mcp/plugins/vuln_review_test.go
+++ internal/mcp/plugins/vuln_review_test.go
@@ -5,15 +5,14 @@ import (
 	"github.com/Tencent/AI-Infra-Guard/internal/gologger"
 	"github.com/Tencent/AI-Infra-Guard/internal/mcp/models"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
 func TestVulnReview(t *testing.T) {
-	plugin := VulnReviewPlugin("")
-	codePath := "/Users/python/PycharmProjects/pythonProject/mcp_test_case/mcps/Asteria"
-	model := "Qwen/Qwen3-32B"
-	token := "empty"
-	baseUrl := "http://11.220.10.79:8080/v1"
+	dd, err := os.ReadFile("test.json")
+	assert.NoError(t, err)
+	plugin := VulnReviewPlugin(string(dd))
 	aiModel := models.NewOpenAI(token, model, baseUrl)
 	config := &McpPluginConfig{
 		CodePath:    codePath,
