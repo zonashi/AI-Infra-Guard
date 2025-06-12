a82adb0edc0c02bd4b00b288500375fc428da8ff
zhuque <zhuque@tencent.com>
2025-06-12 17:22:39 +0800

    添加utils_test测试用例

--- internal/mcp/utils/utils_test.go
+++ internal/mcp/utils/utils_test.go
@@ -6,7 +6,39 @@ import (
 )
 
 func TestListDir(t *testing.T) {
-	sb, err := ListDir("/Users/python/workspace/AI-Infra-Guard", 1)
+	sb, err := ListDir("/mcp-server", -1, "")
+	assert.NoError(t, err)
+	t.Log(sb)
+}
+
+func TestGrepFile(t *testing.T) {
+	sb, err := Grep("/mcp-server/src/mcp_server/server.py", "@mcp\\.tool.*\n.*def", 3)
+	assert.NoError(t, err)
+	t.Log(sb)
+}
+
+func TestGrepDirectory(t *testing.T) {
+	sb, err := Grep("/mcp-server", "AppConfig", 3)
+	assert.NoError(t, err)
+	t.Log(sb)
+	p := "SSE|streamable-http|EventSource"
+	sb, err = Grep("/mcp-server", p, 3)
+	assert.NoError(t, err)
+	t.Log(sb)
+}
+
+func TestReadBigFile(t *testing.T) {
+	sb, err := ReadFileChunk("/mcp-server/src/mcp_server/server.py", 0, 0, 10*1024)
+	assert.NoError(t, err)
+	t.Log(sb)
+
+	sb, err = ReadFileChunk("/mcp-server/src/mcp_server/server.py", 0, 2, 10*1024)
+	assert.NoError(t, err)
+	t.Log(sb)
+}
+
+func TestReadSmallFile(t *testing.T) {
+	sb, err := ReadFileChunk("/mcp-server/src/mcp_server/app_config.py", 0, 0, 10*1024)
 	assert.NoError(t, err)
 	t.Log(sb)
 }
