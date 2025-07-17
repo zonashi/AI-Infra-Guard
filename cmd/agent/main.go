ffba6c6ab83347f89f64b31cfc647ccd2b9f9139
zhuque <zhuque@tencent.com>
2025-07-17 10:16:01 +0800

    docker集成适配

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -5,26 +5,39 @@ import (
 	"fmt"
 	"github.com/Tencent/AI-Infra-Guard/common/agent"
 	"github.com/Tencent/AI-Infra-Guard/internal/gologger"
+	"os"
 )
 
 func main() {
 	var server string
-	flag.StringVar(&server, "server", "21.6.190.156:8088", "server")
+	flag.StringVar(&server, "server", "", "server")
 	flag.Parse()
+	if server == "" {
+		v := os.Getenv("AIG_SERVER")
+		if v != "" {
+			server = v
+		}
+	}
+	if server == "" {
+		gologger.Errorln("server is empty")
+		return
+	}
 
 	serverUrl := fmt.Sprintf("ws://%s/api/v1/agents/ws", server)
 
 	x := agent.NewAgent(agent.AgentConfig{
 		ServerURL: serverUrl,
 		Info: agent.AgentInfo{
-			ID:       "test",
-			HostName: "test",
-			IP:       "127.0.0.1",
+			ID:       "test_id",
+			HostName: "test_hostname",
+			IP:       "localhost",
 			Version:  "0.1",
 			Metadata: "",
 		},
 	})
-	agent2 := agent.AIInfraScanAgent{}
+	agent2 := agent.AIInfraScanAgent{
+		Server: server,
+	}
 	agent3 := agent.McpScanAgent{Server: server}
 	agent4 := agent.ModelJailbreak{}
 	agent5 := agent.ModelRedteamReport{Server: server}
